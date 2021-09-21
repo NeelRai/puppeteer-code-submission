6e9360fb4398d83bbd047c07f295a23a40c0c4a6
Neel <neelrai906@gmail.com>
2021-09-21 13:21:52 +0530

    added opening site basics

--- intro.js
+++ intro.js
@@ -1,1 +1,12 @@
-console.log("abcd");
+const puppeteer =require('puppeteer');
+
+let browserOpen=puppeteer.launch({
+    headless: false,
+    args :['--start-maximized'],
+    defaultViewport:null
+})
+//opening new page i.e 2 tabs
+browserOpen.then(function(browserObj){
+    let BrowserOpenPromise=browserObj.newPage()
+    return BrowserOpenPromise;
+})
