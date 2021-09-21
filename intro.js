a6b4c0540f411aaf6fcb7ebe12430530ac0696b8
Neel <neelrai906@gmail.com>
2021-09-21 15:42:54 +0530

    tried to enter text in logic -failed

--- intro.js
+++ intro.js
@@ -1,12 +1,28 @@
-const puppeteer =require('puppeteer');
+const puppeteer = require('puppeteer');
+
+const loginLink = 'https://www.hackerrank.com/auth/login'
+const email = 'yiger16363@vefblogg.com'
+const password = '123456'
 
 let browserOpen=puppeteer.launch({
     headless: false,
     args :['--start-maximized'],
     defaultViewport:null
 })
+let page
+
 //opening new page i.e 2 tabs
 browserOpen.then(function(browserObj){
     let BrowserOpenPromise=browserObj.newPage()
     return BrowserOpenPromise;
+}).then(function(newTab){
+    page=newTab
+    let hackerRankOpenPromise=newTab.goto(loginLink)
+    return hackerRankOpenPromise
+ }).then(function(){
+    let emailIsEntered= page.type("input[id='input=1']", email, {delay:50})
+    return emailIsEntered
+}).then(function(){
+    let passwordIsEntered=page.type("input[type='password']",password, {delay :50})
+    return passwordIsEntered
 })
